82abdc4db6a222d76e8a209d986a5d57b12f783d
Carrot97 <lixuhuiustb@163.com>
2020-12-02 10:14:04 +0800

    导航条改选中状态改为模板引擎控制

--- src/main/resources/static/assets/js/nav_switch_handler.js
+++ src/main/resources/static/assets/js/nav_switch_handler.js
@@ -1,19 +1,5 @@
-$(function () {
-    $('.main-nav li:first').addClass('active')
-})
-
+// 使已经选取的标签不可再点击
 $('.main-nav li a').click(function () {
     if ($(this).parent().hasClass('active')) {
         return false
-    }
-    $(".main-nav").find("li").each(function () {
-        $(this).removeClass("active");
-    });
-    $(this).parent().addClass('active')
-})
-
-$('.navbar-right li a').click(function () {
-    $(".main-nav").find("li").each(function () {
-        $(this).removeClass("active");
-    });
-})
+    }})
